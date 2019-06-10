992efa5fc40b3382db6e98dacd71083217390187
Valentina Núñez <vanu.24@hotmail.com>
2019-06-09 23:40:08 -0500

    Create Contents.swift

---
+++ Juego de memoria .playground/Contents.swift
@@ -0,0 +1,16 @@
+for i in 0...100{
+    if i % 5 == 0 {
+        print(" # \(i) Bingo!!!")
+    }
+    if i % 2 == 0 {
+        print(" # \(i) Par!!!")
+    }
+    if i % 2 != 0 {
+        print(" # \(i) Impar!!!")
+    }
+    if i >= 30 && i <= 40 {
+        print(" # \(i) Viva Swift!!!")
+    }
+}
+
+
